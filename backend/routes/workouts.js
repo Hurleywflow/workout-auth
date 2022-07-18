bd307dcad003d1626e76b9759b7e0cfd7f40128f
Hurley <86563399+Hurleywflow@users.noreply.github.com>
2022-07-18 16:46:08 +1000

    Add mongoose and connect to database

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -8,5 +8,28 @@ router.get('/', (req, res) => {
 });
 
 
+// Get a single workout
+router.get('/:id', (req, res) => {
+  res.json({ message: 'GET a single Workout route' });
+}
+);
+
+// Post a new single workout
+router.post('/', (req, res) => {
+  res.json({ message: 'POST a new Workout route' });
+}
+);
+
+// Delete a single workout
+router.delete('/:id', (req, res) => {
+  res.json({ message: 'DELETE a single Workout route' });
+}
+);
+
+// Update a single workout
+router.patch('/:id', (req, res) => {
+  res.json({ message: 'UPDATE a single Workout route' });
+}
+);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,31 @@
+// @ts-nocheck
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutsRoutes = require('./routes/workouts');
 
 // Create a new express application instance
 const app = express();
 
-
 // Create a new middleware instance
+app.use(express.json());
 app.use((req, res, next) => {
-  console.log('Request URL:', req.url, req.path, req.method);
+  console.log('Request URL:', req.path, req.method);
   next();
 });
 
 // Route and specific path 'api/workouts/<now is all workoutsRoutes>'
-app.use('api/workouts', workoutsRoutes);
+app.use('/api/workouts', workoutsRoutes);
 
-// Listen on port 4000
-app.listen(process.env.PORT, () => {
-  console.log('Server listening on port: ', process.env.PORT);
-});
+// Connect to to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen fro request endpoint
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to db and listening on port: ', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log('Error connecting to database:', err);
+  });
